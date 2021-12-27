0ab79fb7ffafad08a4fb3ea2c2f80aad5bac2f69
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2021-12-27 21:26:29 +0100

    fixed details for desktop

--- script.js
+++ script.js
@@ -9,22 +9,10 @@ let cookie = `
 <button>Show</button>
 </section>`;
 
-// function checkScroll() {
-//     console.log('checkScroll');
-//     if (
-//         document.body.scrollTop >= 1 ||
-//         document.documentElement.scrollTop >= 1
-//     ) {
-//         console.log('scroll');
-//     }
-// }
-
-// checkScroll();
 const header = document.querySelector('header');
 
 window.onscroll = () => {
     if (window.scrollY > 75) {
-        console.log('scroll');
         header.classList.add('scroll-active');
     } else {
         header.classList.remove('scroll-active');
