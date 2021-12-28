ea552886eb969285c694629c6eaab5cf1fb238f2
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2021-12-28 10:20:19 +0100

    added svg & g and animation

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-console.log('Hello');
-
+// COOKIE
 let main = document.querySelector('main');
-
 let cookie = `
 <section>
 <p>Text</p>
@@ -9,7 +7,9 @@ let cookie = `
 <button>Show</button>
 </section>`;
 
+// CHANGE HEADER COLOR
 const header = document.querySelector('header');
+const desktopHeader = document.querySelector('.desktop-menu');
 
 window.onscroll = () => {
     if (window.scrollY > 75) {
@@ -18,3 +18,26 @@ window.onscroll = () => {
         header.classList.remove('scroll-active');
     }
 };
+
+if (window.innerWidth > 900) {
+    window.onscroll = () => {
+        if (window.scrollY > 75) {
+            desktopHeader.classList.add('scroll-active');
+        } else {
+            desktopHeader.classList.remove('scroll-active');
+        }
+    };
+}
+
+// SVG HEART ANIMATION
+gsap.to('#svgHeart', {
+    duration: 1,
+    scale: 1.2,
+    repeat: 10,
+});
+
+gsap.to('#svgHeartDesktop', {
+    duration: 1,
+    scale: 1.2,
+    repeat: 10,
+});
