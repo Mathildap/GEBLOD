146cd3d6bb3f82e5c08c98504e0b309bf0f03a28
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2021-12-30 08:57:21 +0100

    change svg animation to 10 times

--- script.js
+++ script.js
@@ -76,14 +76,14 @@ document.getElementById('xIcon').addEventListener('click', () => {
 gsap.to('#svgHeart', {
     duration: 1,
     scale: 1.2,
-    repeat: -1,
+    repeat: 10,
 });
 
 // SVG HEART ANIMATION DESKTOP
 gsap.to('#svgHeartDesktop', {
     duration: 1,
     scale: 1.2,
-    repeat: -1,
+    repeat: 10,
 });
 
 // COOKIES
